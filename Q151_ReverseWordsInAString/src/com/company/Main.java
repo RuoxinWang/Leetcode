3bb812a61cb2b771f12f9d860cddd350f0b3b196
RuoxinWang <rwang0104@gmail.com>
2021-10-12 16:25:21 -0700

    Q151 Reverse Words in a String

--- Q151_ReverseWordsInAString/src/com/company/Main.java
+++ Q151_ReverseWordsInAString/src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 public class Main {
+    public Main() {
+    }
 
     public static void main(String[] args) {
-	    String s1 = "the sky is blue";
+        String s1 = "the sky is blue";
         String s2 = "  hello world   ";
         String s3 = "a good   example";
         String s4 = "  Bob    Loves  Alice   ";
@@ -19,9 +21,10 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         String[] words = s.trim().split("\\s+");
 
-        for(int i = words.length -1; i >= 0; i--){
+        for(int i = words.length - 1; i >= 0; --i) {
             sb.append(words[i] + " ");
         }
+
         return sb.toString().trim();
     }
 }
