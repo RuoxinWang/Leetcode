1ef69259a9b7d6303cde69a2e968ead18dcbe71c
RuoxinWang <rwang0104@gmail.com>
2021-10-21 22:03:12 -0700

    Q88 Merge Sorted Array

---
+++ Q88_MergeSortedArray/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        int[] nums3 = {1};
+        int[] nums4 = {};
+        int[] nums5 = {0};
+        int[] nums6 = {1};
+        merge(nums1, 3, nums2, 3);
+        merge(nums3,1,nums4,0);
+        merge(nums5,0,nums6,1);
+
+
+    }
+    private static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int end = m + n - 1;
+
+        while(m - 1 >= 0 || n - 1 >= 0){
+            int x = m - 1 >= 0 ? nums1[m - 1] : Integer.MIN_VALUE;
+            int y = n - 1 >= 0 ? nums2[n - 1] : Integer.MIN_VALUE;
+
+            if(x > y){
+                nums1[end--] = x;
+                m--;
+            }else{
+                nums1[end--] = y;
+                n--;
+            }
+        }
+
+    }
+}
