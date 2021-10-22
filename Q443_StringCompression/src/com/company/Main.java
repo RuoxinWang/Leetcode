7b52e0ea9afc1d63f0ad7cbee654ef3aa3073a26
RuoxinWang <rwang0104@gmail.com>
2021-10-22 15:04:31 -0700

    Q443 String Compression

--- Q443_StringCompression/src/com/company/Main.java
+++ Q443_StringCompression/src/com/company/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	char[] chars1 = {'a','a','b','b','c','c','c'};
+	    char[] chars1 = {'a','a','b','b','c','c','c'};
         char[] chars2 = {'a'};
         char[] chars3 = {'a','b','b','b','b','b','b','b','b','b','b','b','b'};
         char[] chars4 = {'a','a','a','b','b','a','a'};
